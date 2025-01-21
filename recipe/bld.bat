352cb9e4f20c612d6cdeb79e13533c6a7bdcded4
Nicholas Bollweg <nick.bollweg@gmail.com>
2025-01-21 08:09:16 -0600

    revert bat changes (probably windows quoting issues)

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -7,24 +7,25 @@ cd rust
 cargo install --locked --root "%PREFIX%" --path . || goto :error
 
 :: move to scripts
-md "%SCRIPTS%" || echo "%SCRIPTS% already exists"
-move "%PREFIX%\bin\selenium-manager.exe" "%SCRIPTS%"
+md %SCRIPTS% || echo "%SCRIPTS% already exists"
+move %PREFIX%\bin\selenium-manager.exe %SCRIPTS%
 
-cargo-bundle-licenses --format yaml --output "%SRC_DIR%\THIRDPARTY.yml"
+cargo-bundle-licenses --format yaml --output %SRC_DIR%\THIRDPARTY.yml
 
 :: remove extra build files
 del /F /Q "%PREFIX%\.crates2.json"
 del /F /Q "%PREFIX%\.crates.toml"
 
+
 :: Copy the [de]activate scripts to %PREFIX%\etc\conda\[de]activate.d.
 :: This will allow them to be run on environment activation.
 FOR %%F IN (activate deactivate) DO (
-    if not exist "%PREFIX%\etc\conda\%%F.d" MKDIR "%PREFIX%\etc\conda\%%F.d"
+    if not exist %PREFIX%\etc\conda\%%F.d MKDIR %PREFIX%\etc\conda\%%F.d
     if errorlevel 1 exit 1
-    copy "%RECIPE_DIR%\scripts\%%F.bat" "%PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.bat"
+    copy %RECIPE_DIR%\scripts\%%F.bat %PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.bat
     :: We also copy .sh scripts to be able to use them
     :: with POSIX CLI on Windows.
-    copy "%RECIPE_DIR%\scripts\%%F-win.sh" "%PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.sh"
+    copy %RECIPE_DIR%\scripts\%%F-win.sh %PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.sh
     if errorlevel 1 exit 1
 )
 
